860ceca8d26eb05ae5a143325d3b5f56c6c2913b
Rijul Poudel <rijulpoudel72@gmail.com>
2026-07-27 16:03:40 -0500

    [refactor]: switch to prisma-client-js generator with adapter setup

--- backend/prisma.js
+++ backend/prisma.js
@@ -1,7 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+import pkg from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
+const { PrismaClient } = pkg;
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
 });
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
